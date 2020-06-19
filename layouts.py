acd4765226cec4b4e1a98472680fb30e3ebde08a
remsgss <saout@hotmail.fr>
2020-06-19 11:04:01 +0200

    modif requirements.txt 3

--- layouts.py
+++ layouts.py
@@ -36,9 +36,13 @@ features2 = df2.columns.drop(['id'])
 username = 'LesCharentes'
 
 fichiers_chemin_donnees = glob.glob("./data/*.xlsx")
+print(fichiers_chemin_donnees,'\n')
 fichiers_interet = [str for str in fichiers_chemin_donnees if str.startswith('.\\data\\df_tweets_'+username)]
+print(fichiers_interet,'\n')
 dates_fichiers_interet = [str.strip('./data/df_tweets_'+username).strip('.xlsx') for str in fichiers_interet]
+print(dates_fichiers_interet,'\n')
 dates_fichiers_interet = [dt.datetime.strptime(date, "%b-%d-%Y").date() for date in dates_fichiers_interet]
+print(dates_fichiers_interet,'\n')
 max_date = max(dates_fichiers_interet)
 del(fichiers_chemin_donnees,fichiers_interet,dates_fichiers_interet)
 
