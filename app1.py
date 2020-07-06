60d1f73bc28e4360b1a114947ef4e450149d1155
remsgss <saout@hotmail.fr>
2020-07-06 09:49:54 +0200

    ajout page 2

--- app1.py
+++ app1.py
@@ -62,7 +62,7 @@ nb_retweet_hashtags = pd.read_excel('.\\data\\nb_retweets_'+username+'_'+max_dat
 ######################################### APPLICATION ############################################
 # ouverture de l'appli
 app = dash.Dash(__name__)
-auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS
+auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
 
 layout = dict(
